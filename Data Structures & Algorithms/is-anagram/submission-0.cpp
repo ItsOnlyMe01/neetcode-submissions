f63e9e9174d72e1f57ca0812f224602a86808327
Ritesh <134477804+ItsOnlyMe01@users.noreply.github.com>
2026-09-17 21:04:44 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if (s.length() != t.length())
+            return false;
+
+        unordered_map<char, int> freq;
+
+        for (char c : s) {
+            freq[c]++;
+        }
+
+        for (char c : t) {
+            if (freq.find(c) == freq.end())
+                return false;
+
+            freq[c]--;
+
+            if (freq[c] == 0)
+                freq.erase(c);
+        }
+
+        return freq.empty();
+    }
+};
